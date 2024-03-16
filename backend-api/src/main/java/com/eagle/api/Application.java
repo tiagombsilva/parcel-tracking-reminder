9a1fe0b24c4079c8a94010a014d095a8181a7ae0
Tiago Silva <poketiago8@hotmail.com>
2024-03-16 20:37:56 -0100

    SpringBoot skeleton and docker dev/prd files running

--- backend-api/src/main/java/com/eagle/api/Application.java
+++ backend-api/src/main/java/com/eagle/api/Application.java
@@ -1,13 +1,11 @@
-package com.eagle.parceltracking.api;
+package com.eagle.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
--- backend-api/src/test/java/com/eagle/api/ApplicationTests.java
+++ backend-api/src/test/java/com/eagle/api/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eagle.parceltracking.api;
+package com.eagle.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
