ef61d41d319d45f07d73128a4fd7e245f4886dbf
Tiago Silva <poketiago8@hotmail.com>
2024-04-09 21:20:58 +0000

    finishing API main endpoints
    Signed-off-by: Tiago Silva <poketiago8@hotmail.com>

--- backend-api/src/main/java/com/eagle/api/parcel/ParcelController.java
+++ backend-api/src/main/java/com/eagle/api/parcel/ParcelController.java
@@ -5,7 +5,11 @@ import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Optional;
