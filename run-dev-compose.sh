8abe1cfb68f61e85cc015a633cdccb0f18a3d5c9
Tiago Silva <poketiago8@hotmail.com>
2024-07-20 17:42:38 +0000

    Making dev working by just running run-dev-compose.sh script
    Signed-off-by: Tiago Silva <poketiago8@hotmail.com>

--- run-dev-compose.sh
+++ run-dev-compose.sh
@@ -3,8 +3,13 @@
 echo "Stopping container and remove image"
 docker-compose -f docker-compose-dev.yml down > /dev/null | true
 
+echo "building project"
+cd backend-api/
+./mvnw install -DskipTests
+
 echo "Starting Services"
-docker-compose -f docker-compose-dev.yml up -d
+cd ../
+docker-compose -f docker-compose-dev.yml up --build -d
 
 echo "Attaching to API"
 docker logs -f backend-api
