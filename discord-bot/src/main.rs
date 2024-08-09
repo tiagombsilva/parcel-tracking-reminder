6233a179c4daa509f11ef55aecc300c83cb2f8fe
Tiago Silva <poketiago8@hotmail.com>
2024-08-09 20:39:24 +0000

    Rust cleanup and initialization of RabbitMQ message broker to notify rust discord bot

--- discord-bot/src/main.rs
+++ discord-bot/src/main.rs
@@ -1,13 +1,4 @@
-mod parcels;
-
-use parcels::ParcelsServiceClient;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = ParcelsServiceClient::new("http://[::1]:9090").await?;
-
-    let parcels = client.get_parcels().await?;
-
-    println!("Received parcels: {:?}", parcels);
     Ok(())
 }
