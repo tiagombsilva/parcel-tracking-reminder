01322b18c0770cd61163a6f5f7b224ec07cb20dc
Tiago <poketiago8@hotmail.com>
2025-02-01 20:25:37 -0100

    fixing docker compose

--- run-dev-compose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Stopping container and remove image"
-docker compose -f docker-compose-dev.yml down > /dev/null | true
-
-echo "Starting Services"
-cd ../
-docker compose -f docker-compose-dev.yml up --build -d
